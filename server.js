3c7edbb301e7ae5fded8669de458c56aca63e223
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-08-17 13:42:08 +0700

    add cluster with max logical physical core

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const cluster = require('cluster');
+const os = require('os');
 const app = express();
 
 function delay(duration) {
@@ -17,13 +18,26 @@ app.get('/', (req, res) => {
   // crypto modules https://nodejs.org/api/crypto.html
   // server cant be started when use this but this only take a few seconds
 
-  res.send('Performance examples');
+  res.send(`Performance example ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   // delay the response
   delay(9000);
-  res.send('Ding ding ding!');
+  res.send(`Ding ding ding! ${process.pid}`);
 });
 
-app.listen(3000);
+console.log(`Running server.js`);
+
+if (cluster.isMaster) {
+  console.log(`Master has been started...`);
+  const NUM_WORKERS = os.cpus().length;
+  // GET NUMBER OF LOGICAL CORES IN SERVER MACHINE
+  console.log('NUM_WORKERS', NUM_WORKERS);
+  for (let index = 0; index < NUM_WORKERS; index++) {
+    cluster.fork();
+  }
+} else {
+  console.log('Worker process started...');
+  app.listen(3000);
+}
